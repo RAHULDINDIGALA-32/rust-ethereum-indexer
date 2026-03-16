07f79ec124ee037e353b9cf5d1311731a97f517c
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2026-03-16 18:20:25 +0530

    feat: Implement the core block-logs ingestion primitive

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use alloy::providers::{Provider, ProviderBuilder, RootProvider};
+use alloy::rpc::types::{Filter, Log};
+use alloy::primitives::{Address};
+use alloy::transports::http::{Http, Client};
+
+use std::sync::Arc;
+
+pub struct RpcClient {
+    provider: Arc<RootProvider<Http<Client>>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl RpcClient {
+    pub async fn new(rpc_url: &str) -> Self {
+
+        let provider = ProviderBuilder::new()
+            .on_http(rpc_url.parse().unwrap());
+
+        Self {
+            provider: Arc::new(provider),
+        }
+    }
+
+    pub async fn fetch_logs(
+        &self,
+        from_block: u64,
+        to_block: u64,
+        contract: Address,
+    ) -> Result<Vec<Log>, Box<dyn std::error::Error>> {
+
+        let filter = Filter::new()
+            .address(contract)
+            .from_block(from_block)
+            .to_block(to_block);
+
+        let logs = self.provider.get_logs(&filter).await?;
+
+        Ok(logs)
     }
 }
--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use rpc::RpcClient;
+use alloy::primitives::address;
+
+
+#[tokio::main]
+async fn main() {
+
+    let rpc_url = "https://eth-sepolia.g.alchemy.com/v2/<YOUR_ALCHEMY_API_KEY>";
+    
+    let client = RpcClient::new(rpc_url).await;
+
+    let contract = address!("0x1c7d4b196cb0c7b01d743fbc6116a902379c7238"); // Sepolia USDC contract address
+
+    let logs = client
+        .fetch_logs(10457550, 10457558, contract)
+        .await
+        .expect("Failed to fetch logs");
+        
+    println!("logs fetched: {}", logs.len());
+
+    for log in logs {
+        println!("Txn: {:?}", log.transaction_hash);
+        println!("Topics: {:?}", log.topics());
+        println!("Data: {:?}", log.data());
+        println!("-----------------------------");
+    }
 }
